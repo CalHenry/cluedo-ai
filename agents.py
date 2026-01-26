9fd357823a3252a766cd3b6c9424f6349affb9ba
Calixte Henry <131826512+CalHenry@users.noreply.github.com>
2026-01-26 19:55:19 +0100

    doc: updated agent.py docstring

--- agents.py
+++ agents.py
@@ -2,21 +2,24 @@ from pydantic_ai import Agent
 from pydantic_ai.models.openai import OpenAIChatModel
 from pydantic_ai.providers.openai import OpenAIProvider
 
-# 3 agents:
-# - supervisor - he decides, can validate a guess and return the fibnal answer
-# - researcher - he can use the tools to query informations
-# - processor - he processes informations passed by the supervisor
+"""
+3 agents:
+- supervisor: he decides, can validate a guess and return the fibnal answer
+- researcher: he can use the tools to query informations
+- processor: he processes informations passed by the supervisor
 
-# I use LM studio to be able to use a MLX model with pydantic-ai. LM studio provide a OpenAI compatible API
-# 2 actions to start lm studio (in the terminal):
-# - 'lms server start'
-# To see if the server is running you can do 'curl http://localhost:1234/v1/models'. It should return the list of the downloaded models - can help find the exact name of the model
-# - 'lms load <model_name>'
-# (optional) - 'lms ps' to see the loaded model
+I use LM studio to be able to use a MLX model with pydantic-ai. LM studio provide a OpenAI compatible API
+2 actions to start lm studio (in the terminal):
+- 'lms server start'
+To see if the server is running you can do 'curl http://localhost:1234/v1/models'. It should return the list of the downloaded models - can help find the exact model name
+- 'lms load <model_name>'
+(optional) - 'lms ps' to see the loaded model
+Use the GUI if you're not comfortable with the cli, it can all be done there + has server logs
+"""
 
 # Supervisor Agent - orchestrates workflow
 supervisor_model = OpenAIChatModel(
-    model_name="lfm2.5-1.2b-instruct-mlx",  # model name has to be filled but a the actual string do not matter, only the correct url is required)
+    model_name="lfm2.5-1.2b-instruct-mlx",  # model name has to be filled but the actual name do not matter, only the correct url is required)
     provider=OpenAIProvider(
         base_url="http://127.0.0.1:1234/v1",
         api_key="lm_studio",  # also useless but emphasize we use LM studio
