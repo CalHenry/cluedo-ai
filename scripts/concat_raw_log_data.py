8fd38ef16b5233d4c05393da2cd8feed9f19db83
Calixte Henry <131826512+CalHenry@users.noreply.github.com>
2026-02-27 16:40:55 +0100

    feat: logs are now concatenated before being processed
    concat_raw_logs.py takes raw logs files and return them as a single
    parquet file in data/interim/ to be processed by processed_logs.py
    process_logs.py now checks if the files in interim have been processed
    looking in data/process if the counterpart exist.
    This way we can process new logs with no duplicates and without
    processing them more than once.

---
+++ scripts/concat_raw_log_data.py
@@ -0,0 +1,15 @@
+from datetime import datetime
+from pathlib import Path
+
+import polars as pl
+
+interim_dir = Path("data/interim")
+
+# Current date and time is a easy way to give a unique name to the files
+timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+output_path = interim_dir / f"concat_{timestamp}.parquet"
+
+lfs = pl.scan_parquet("data/raw/*.parquet")
+lf = lfs.unique()
+
+lf.sink_parquet(output_path)
